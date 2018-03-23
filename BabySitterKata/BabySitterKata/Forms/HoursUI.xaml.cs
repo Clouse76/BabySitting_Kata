692b432f6901add44eb127434dd6813e5b85e69d
Clouse76 <clouse.matt@gmail.com>
2018-03-22 21:11:44 -0400

    This is my first check in.
    Well this is my first check in of the Kata.  I kinda realize now I should have done this a while ago so you can see the progression.  At the time I was more concerned with the Kata.  while checking in the folder I see that the OBJ file was included.  TFS leaves these out by default. sorry for the inclusion.

---
+++ BabySitterKata/BabySitterKata/Forms/HoursUI.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
+namespace BabySitterKata
+{
+    /// <summary>
+    /// This function of this program is to calculate the nightly charge for a babysitter.
+    /// The buisness rules are:
+    /// starts no earlier than 5:00PM
+    /// leaves no later than 4:00AM
+    /// gets paid $12/hour from start-time to bedtime
+    /// gets paid $8/hour from bedtime to midnight
+    /// gets paid $16/hour from midnight to end of job
+    /// gets paid for full hours(no fractional hours)
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool endPM = false;
+        bool bedPM = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This Function Takes the values input on the UI checks for the validity of the data.
+        /// if everything checks out it passes the data to the HoursCalculator class and returns the hours and pay earned.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCalculate_Click(object sender, RoutedEventArgs e)
+        {
+            if(CheckForBadData() == false)
+            {   
+                HoursCalculator DailyResults = new HoursCalculator();
+                DailyResults.CalculatePay(txtStartTime.Text, txtEndTime.Text, txtBedTime.Text, endPM, bedPM);
+                lblBedHours.Content = DailyResults.TotalBedTime;
+                lblstandardHours.Content = DailyResults.TotalStandardTime;
+                lblOvertimeHours.Content = DailyResults.TotalOverTime;
+                lblTotalHours.Content = DailyResults.TotalBedTime + DailyResults.TotalStandardTime + DailyResults.TotalOverTime;
+                lblGrossPay.Content = DailyResults.StandardTimePrice + DailyResults.BedTimePrice + DailyResults.OverTimePrice;
+                lblstandardPay.Content = DailyResults.StandardTimePrice;
+                lblOvertimePay.Content = DailyResults.OverTimePrice;
+                lblBedPay.Content = DailyResults.BedTimePrice;
+                endPM = false;
+                bedPM = false;
+            }
+        }
+
+        /// <summary>
+        /// This Button closes the application.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        /// <summary>
+        /// This function makes sure that all of the user imput is in the correct format and logically accurate.
+        /// will return true if any issues are detected.  there is a great deal of code here and has an oppertunity to be refactored.
+        /// </summary>
+        private bool CheckForBadData()
+        {
+            //Create a stringbuilder to hold all the potential issues.
+            StringBuilder issues = new StringBuilder();
+            bool goodStart = true, goodEnd = true, goodBed = true;
+
+            //check to see if the time is in a valid format inform the user wich one is incorrect. mark them as false if they are bad so further calculations will not occur.
+            if (IsValidTime(txtStartTime.Text) == false)
+            {
+                issues.AppendLine("Start Time is not in the correct time format.");
+                goodStart = false;
+            }
+            if(txtBedTime.Text.Length > 0 && IsValidTime(txtBedTime.Text) == false)
+            {
+                issues.AppendLine("Bed Time is not in the correct time format.");
+                goodBed = false;
+            }
+            if(IsValidTime(txtEndTime.Text) == false)
+            {
+                issues.AppendLine("End Time is not in the correct time format.");
+                goodEnd = false;
+            }
+
+            //make sure we have good formatting before checking if they are logically correct.
+            if(goodStart && goodEnd)
+            {
+                TimeSpan startTime = TimeSpan.Parse(txtStartTime.Text);
+                startTime = startTime.Add(new TimeSpan(12, 0, 0));                      //put the start time in the 24-hour format
+                TimeSpan endTime = TimeSpan.Parse(txtEndTime.Text);
+                if (rbEndPM.IsChecked == true)
+                {
+                    endTime = endTime.Add(new TimeSpan(12, 0, 0));                      //if "PM" is selected put end time in the 24-hour format
+                    endPM = true;
+                }
+                if(startTime < new TimeSpan(17, 0, 0))
+                {
+                    issues.AppendLine("Start time cannot be before 5pm");
+                }
+                if(endTime < new TimeSpan(17,0,0) && endTime > new TimeSpan(4, 0, 0))   // make sure end time is less than 4am and greater than 5pm.
+                {
+                    issues.AppendLine("End time can't be past 4AM or before 5PM");
+                }
+                //Check to see if there is a valid bed time and make sure it happens durring the shift.
+                if (goodBed && txtBedTime.Text.Length > 0)
+                {
+                    TimeSpan bedTime = TimeSpan.Parse(txtBedTime.Text);
+                    if(rbBedPM.IsChecked == true)                                       //if "PM" is selected put end time in the 24-hour format
+                    {
+                        bedTime = bedTime.Add(new TimeSpan(12, 0, 0));
+                        bedPM = true;
+                    }
+                    if((bedTime < startTime && bedTime > new TimeSpan(4,0,0)) || (bedTime > endTime && endTime > new TimeSpan(4,0,0)))
+                    {
+                        issues.AppendLine("Please enter a valid bed time. It has to be durring your shift.");
+                    }
+                }
+            }
+
+            //is there are issues then show them and return false.
+            if (issues.Length > 0)
+            {
+                MessageBox.Show(issues.ToString(), "The Following issues need resolved.", MessageBoxButton.OK);
+                return true;
+            }
+            else
+            {
+                //we are good to go.
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// This Function checks to make sure a valid time is entered.  
+        /// will return "true" if the string is in a proper time format, "false" if it isn't 
+        /// </summary>
+        /// <param name="thetime"></param>
+        /// <returns></returns>
+        public bool IsValidTime(string thetime)
+        {
+            Regex checktime =
+                new Regex(@"^(?:0?[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$");
+
+            return checktime.IsMatch(thetime);
+        }
+    }
+}
---
+++ BabySitterKata/BabySitterKata/Modules/HoursCalculator.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BabySitterKata
+{
+    /// <summary>
+    /// this class performs the calculations for the work done.  
+    /// There could be an arguement made to move alot of the data validations to this class. 
+    /// </summary>
+    class HoursCalculator
+    {
+        public int TotalStandardTime = 0;
+        public int TotalBedTime = 0;
+        public int TotalOverTime = 0;
+        public double StandardTimePrice = 0;
+        public double BedTimePrice = 0;
+        public double OverTimePrice = 0;
+
+        //I figure these should become adjustable at some point.
+        private double standardRate = 12;
+        private double bedRate = 8;
+        private double overTimeRate = 16;
+
+        public void CalculatePay(string startTime, String endTime, String bedTime, bool EndPM, bool BedPM)
+        {
+            TimeSpan startTimeSpan = TimeSpan.Parse(startTime);
+            startTimeSpan = startTimeSpan.Add(new TimeSpan(12, 0, 0));
+            TimeSpan endTimeSpan = TimeSpan.Parse(endTime);
+            if (EndPM)
+            {
+                endTimeSpan = endTimeSpan.Add(new TimeSpan(12, 0, 0));
+            }
+            int totalBedTime = 0;
+            int totalStandardTime = 0;
+            int totalOverTime = 0;
+            if (bedTime.Length > 0)                                                                               //check to see if the kid went to bed.
+            {
+                TimeSpan bedTimespan = TimeSpan.Parse(bedTime);
+                if (BedPM)
+                {
+                    bedTimespan = bedTimespan.Add(new TimeSpan(12, 0, 0));
+                }
+                if(endTimeSpan < new TimeSpan(24, 0, 0) && endTimeSpan > new TimeSpan(4,0,0))                   //Make sure no overtime is due.
+                {
+                    totalBedTime = (endTimeSpan - bedTimespan).Hours;                                           //calculate bedtime before endtime if the dont' make it midnight
+                }
+                else
+                {
+                    if(bedTimespan > new TimeSpan(4, 0, 0))
+                    {
+                        totalBedTime = (new TimeSpan(24, 0, 0) - bedTimespan).Hours;                           //still at work after bed time and after midnight - overtime will be awarded. I mean really what kind of babysitter are you?
+                    }
+                }
+                if(bedTimespan < new TimeSpan(4, 0, 0))                                                        //if the kid didn't get to bed until after midnight bed time doesn't need to calculated.
+                {
+                    totalStandardTime = 7;
+                }
+                else
+                {
+                    totalStandardTime = bedTimespan.Hours - startTimeSpan.Hours;                                //calculate standard time before bed time hours begin.
+                }
+            }
+            else                                                                                               //The kid stayed up.                    
+            {
+                if(endTimeSpan > new TimeSpan(4, 0, 0))
+                {
+                    totalStandardTime = (endTimeSpan - startTimeSpan).Hours;                                   //no bed but ended before midnight
+                }
+                else
+                {
+                    totalStandardTime = 7;                                                                    //no bed before midnight full standard hours earned.
+                }
+            }
+            if(endTimeSpan <= new TimeSpan(4, 0, 0))                                                           //Check for overtime.
+            {
+                totalOverTime = endTimeSpan.Hours;
+            }
+
+            //total up the pay.
+            TotalBedTime = totalBedTime;
+            TotalOverTime = totalOverTime;
+            TotalStandardTime = totalStandardTime;
+            StandardTimePrice = totalStandardTime * standardRate;
+            BedTimePrice = totalBedTime * bedRate;
+            OverTimePrice = totalOverTime * overTimeRate;
+            
+            //assign the results.
+        }
+    }
+}
